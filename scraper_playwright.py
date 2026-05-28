af5d81a71e799f70fa2088954d0e780ee7bf96c8
rifkykurniawan <toettoet.tt@gmail.com>
2026-05-28 13:09:08 +0800

    Add hint and more data

--- scraper_playwright.py
+++ scraper_playwright.py
@@ -4,7 +4,7 @@ import json
 import asyncio
 from playwright.async_api import async_playwright
 
-OUTPUT_FILE = "MELBOURNE_Café_and_Family Dining.csv"
+OUTPUT_FILE = "MELBOURNE_Fine_Dining.csv"
 #Fine_Dining
 #Café_and_Family_Dining
 #Informal_Dining_and_Takeaway
@@ -17,7 +17,7 @@ REGIONS = [
 ]
 
 CATEGORIES = [
-    "cafe",
+    "fine",
     #fine,cafe,takeaway,activity,services,travel
 ]
 
