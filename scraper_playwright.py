859bdb5427761f11a871713c63ead7f4990b6a72
rifkykurniawan <toettoet.tt@gmail.com>
2026-06-01 12:56:41 +0800

    update

--- scraper_playwright.py
+++ scraper_playwright.py
@@ -4,7 +4,7 @@ import json
 import asyncio
 from playwright.async_api import async_playwright
 
-OUTPUT_FILE = "CANBERRA_Fine_Dining.csv"
+OUTPUT_FILE = "vTravel_&_Leisure.csv"
 #Fine_Dining
 #Café_and_Family_Dining
 #Informal_Dining_and_Takeaway
@@ -17,7 +17,7 @@ REGIONS = [
 ]
 
 CATEGORIES = [
-    "fine",
+    "travel",
     #fine,cafe,takeaway,activity,services,travel
 ]
 
